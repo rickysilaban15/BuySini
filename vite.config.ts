6dc0de6ab2462886eb35d77bbcc6ba03b0e77a85
rickysilaban15 <rickysilaban384@gmail.com>
2025-10-08 07:50:31 +0700

    Add vite-tsconfig-paths plugin to fix path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger(), tsconfigPaths()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "./", // ← pastikan base path relatif
-
+  base: "./",
   build: {
     outDir: "dist",
     sourcemap: false,
@@ -37,9 +37,7 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-
   esbuild: {
     drop: mode === "production" ? ["console", "debugger"] : [],
   },
 }));
-  
